70c6adc3277c5507fb65086ede1732afbef7cbd5
felipeleite11 <lipi4shared@hotmail.com>
2020-07-02 17:25:10 -0300

    Excluindo fotos do Slider.

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -12,13 +12,11 @@ import foto6 from '../../assets/img/SLIDER/Ensaio-6.jpg'
 import foto7 from '../../assets/img/SLIDER/Ensaio-7.jpg'
 import foto8 from '../../assets/img/SLIDER/Ensaio-8.jpg'
 import foto9 from '../../assets/img/SLIDER/Ensaio-9.jpg'
-import foto10 from '../../assets/img/SLIDER/Ensaio-10.jpg'
 import foto11 from '../../assets/img/SLIDER/Ensaio-11.jpg'
 import foto12 from '../../assets/img/SLIDER/Ensaio-12.jpg'
 import foto13 from '../../assets/img/SLIDER/Ensaio-13.jpg'
 import foto14 from '../../assets/img/SLIDER/Ensaio-14.jpg'
 import foto15 from '../../assets/img/SLIDER/Ensaio-15.jpg'
-import foto16 from '../../assets/img/SLIDER/Ensaio-16.jpg'
 import foto17 from '../../assets/img/SLIDER/Ensaio-17.jpg'
 import foto18 from '../../assets/img/SLIDER/Ensaio-18.jpg'
 import foto19 from '../../assets/img/SLIDER/Ensaio-19.jpg'
@@ -26,11 +24,11 @@ import foto20 from '../../assets/img/SLIDER/Ensaio-20.jpg'
 import foto21 from '../../assets/img/SLIDER/Ensaio-21.jpg'
 import foto22 from '../../assets/img/SLIDER/Ensaio-22.jpg'
 
-const slides = [foto1, foto2, foto3, foto4, foto6, foto7, foto9, foto11, foto13, foto14, foto15, foto16, foto17, foto18, foto19, foto20, foto21, foto22]
+const slides = [foto1, foto2, foto3, foto4, foto6, foto7, foto9, foto11, foto13, foto14, foto15, foto17, foto18, foto19, foto20, foto21, foto22]
 
 // Adiciona as fotos que ficam adequadas apenas no desktop
 if(window.innerWidth >= 600) {
-	slides.push(foto5, foto8, foto10, foto12)
+	slides.push(foto5, foto8, foto12)
 }
 
 export default function() {
